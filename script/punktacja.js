59401a5a3def91f80d14a4f189ed52dfab336449
spiex <the.combine.harvester@gmail.com>
2019-07-08 22:17:33 +0200

    a

--- script/punktacja.js
+++ script/punktacja.js
@@ -1,3 +1,9 @@
+// const array = [
+//   ["0 - 0", "6", "6", "8", "7.5", "7.5", "10", "10.5", "10.5", "14"],
+//   ["1 - 0", "3", "5", "7", "4.5", "7.5", "10.5", "6", "10", "14"],
+//   ["1 - 1", "6", "6", "8", "7.5", "7.5", "10", "10.5", "10.5", "14"]];
+
+
 
 
 // if (buttons[i].classList.contains('active')) {
@@ -8,21 +14,21 @@ button1.addEventListener('click',()=>{
   button1.classList.add('active');
   button2.classList.remove('active');
   button3.classList.remove('active');
-  checkActive();
+  // checkActive(array);
 });
 
 button2.addEventListener('click',()=>{
   button2.classList.add('active');
   button1.classList.remove('active');
   button3.classList.remove('active');
-  checkActive();
+  // checkActive(array);
 });
 
 button3.addEventListener('click',()=>{
   button3.classList.add('active');
   button2.classList.remove('active');
   button1.classList.remove('active');
-  checkActive();
+  // checkActive(array);
 });
 
 //
