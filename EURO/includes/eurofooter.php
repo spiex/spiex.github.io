26cd85089443ef774825993169f5507231a21d73
spiex <marcin.spiewakowski@gmail.com>
2021-04-23 17:10:17 +0200

    dodanie euro

---
+++ EURO/includes/eurofooter.php
@@ -0,0 +1,10 @@
+</div>
+<!-- end of div.main-wrapper  -->
+
+  <footer>
+    <p><a href='admin.php'><i class='bx bxs-cog'></i>zasady gry</a></p>
+    <p><a href='admin.php'><i class='bx bxs-cog'></i>punktacja</a></p>
+  </footer>
+
+</body>
+</html>
---
+++ EURO/includes/euroheader.php
@@ -0,0 +1,26 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tabelka Euro 2021</title>
+
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+    <!-- ^ BOXICONS -->
+    <link rel="stylesheet" type="text/css" href="./scss/eurostyle.min.css">
+  </head>
+  <body>
+
+  <nav>
+    <div class='logotyp-tabelki'><div><img src='img/logoEURwhite.png'></div><a href="punktacja.php" class='infolink'><i class='bx bx-info-circle' ></i><span class='infotekst'>INFO</span></a></div>
+
+    <ul>
+      <!-- <li><a href="wyniki.php"><img src='img/bannerEURwhite.png'></a></li> -->
+      <li><a href="faza1.php">BARAŻE</a></li>
+      <li><a href="faza2.php">FAZA GRUPOWA</a></li>
+      <li><a href="faza3.php"><i class='bx bxs-trophy'></i>FINAŁ</a></li>
+      <li><a href="statystyki.php"><i class='bx bxs-bar-chart-alt-2' ></i></a></li>
+      <!-- <li><a href="punktacja.php"><i class='bx bxs-calculator'></i>punktacja</a></li> -->
+    </ul>
+  </nav>
+  <div class="main-wrapper">
