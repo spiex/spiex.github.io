aff1e8e498b116eb83e5d0052371e57d0f810975
spiex <marcin.spiewakowski@gmail.com>
2023-09-20 20:00:07 +0200

    raków js

--- lm23/script/main.js
+++ lm23/script/main.js
@@ -51,3 +51,30 @@ for (let i = 0; i < nastepnewyniki.length; i++) {
 
   });
 }
+
+const nastepnewyniki = document.querySelectorAll("#ligaeuropy");
+const nastepnybox = document.querySelectorAll('.bets-box');
+
+for (let i = 0; i < nastepnewyniki.length; i++) {
+  nastepnewyniki[i].addEventListener('click', (e) => {
+
+    // oblicz liczbę typów w danym boksie i ustaw maksymalną wysokość
+    const liczbaTypow = e.target.parentNode.nextElementSibling.querySelectorAll('.bet');
+    const dlugosc = liczbaTypow.length * 24 + 4;
+    // get the mx height of hidden content
+    // liczba typów * 24 px + 4px na dół
+
+      if (e.target.className == "overlay hidden" ) {
+        let box1 = e.target.parentNode;
+        let box2 = box1.nextElementSibling;
+        box2.style.maxHeight = dlugosc + "px";
+        e.target.className = "overlay visible";
+      } else if (e.target.className == "overlay visible" ) {
+        let box1 = e.target.parentNode;
+        let box2 = box1.nextElementSibling;
+        box2.style.maxHeight = "0";
+        e.target.className = "overlay hidden";
+      }
+
+  });
+}
